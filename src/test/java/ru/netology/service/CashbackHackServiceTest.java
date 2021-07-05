8a6e69335c366d659863b58b23d8cf1a236744e9
Ratush Aleksandra <Ne0f1ex>
2021-07-06 01:08:46 +0400

    format

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -9,29 +9,28 @@ public class CashbackHackServiceTest {
     private final CashbackHackService cashbackHackService = new CashbackHackService();
 
     @Test
-    public void remainWithAmountDivisibleAboveBoundary(){
+    public void remainWithAmountDivisibleAboveBoundary() {
         int remain = cashbackHackService.remain(2000);
         assertEquals(remain, 0);
     }
 
     @Test
-    public void remainWithAmountNotDivisibleAboveBoundary(){
+    public void remainWithAmountNotDivisibleAboveBoundary() {
         int remain = cashbackHackService.remain(2324);
         assertEquals(remain, 676);
     }
 
     @Test
-    public void remainWithNotDivisibleAmountLessBoundary(){
+    public void remainWithNotDivisibleAmountLessBoundary() {
         int remain = cashbackHackService.remain(99);
         assertEquals(remain, 901);
     }
 
     @Test
-    public void remainWithAmountEqualsBoundary(){
+    public void remainWithAmountEqualsBoundary() {
         int remain = cashbackHackService.remain(1000);
         assertEquals(remain, 0);
     }
-
 
 
 }
